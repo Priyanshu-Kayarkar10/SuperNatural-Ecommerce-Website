ea0c40442db327f6abdbbdef59fdafbd8f78c95a
Priyanshu <priyanshu.kayarkar009@gmail.com>
2024-07-27 13:40:37 +0530

    Svg Error Resolved

--- src/assets/svgs/AddSvg.tsx
+++ src/assets/svgs/AddSvg.tsx
@@ -7,7 +7,6 @@ const AddSvg = () => {
       viewBox="0 0 22 22"
       width="20"
       xmlns="http://www.w3.org/2000/svg"
-      sx="[object Object]"
       className="css-y880pu"
     >
       <path d="M12 3h-2v7H3v2h7v7h2v-7h7v-2h-7V3z"></path>
